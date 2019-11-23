515272a467545d14f86697a04d72644f46eaf5f8
badogan <basri.dogan.71@gmail.com>
2019-11-23 12:56:36 +0000

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,29 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_accessor :title,:price,:artist,:gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title:,price:,artist:,gallery:)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.total_price
+    #Painting.total_price
+    #Returns an integer that is the total price of all paintings
+    Painting.all.map{|painting|painting.price}.reduce(starting_value=0){|sum,num|sum+num}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,19 @@
 require_relative '../config/environment.rb'
 
+#For the model scribble, check the image in ./tools
 
-
-
+#Input data below
+a_Mike = Artist.new(name:"Mike the Artist",years_experience:10)
+a_Jane = Artist.new(name:"Jane the Artist",years_experience:10)
+a_Aydanur = Artist.new(name:"Aydanur the Artist",years_experience:10)
+g1 = Gallery.new(name:"Warm Gallery",city:"London")
+g3 = Gallery.new(name:"Warmer Gallery",city:"London")
+g2 = Gallery.new(name:"Cold Gallery",city:"Edinburgh")
+a_Mike.create_painting(title:"The Countryside v1",price:9999,gallery_object:g1)
+a_Mike.create_painting(title:"The Countryside v3",price:3330,gallery_object:g3)
+a_Mike.create_painting(title:"The Countryside v2",price:1000,gallery_object:g2)
+a_Aydanur.create_painting(title:"Istanbul Views",price:2000,gallery_object:g1)
+a_Jane.create_painting(title:"Snow Dows",price:3000,gallery_object:g1)
 binding.pry
 
-puts "Bob Ross rules."
+puts "Basri works hard. Learning to play as well..."
