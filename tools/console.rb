dc802755f4ac89b7d89913fdb500f7a76307b26c
Matthew <matthew@Matthews-MacBook-Pro.local>
2019-11-25 09:10:28 +0000

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,7 +4,7 @@ require_relative '../app/models/painting.rb'
 require_relative '../app/models/gallery.rb'
 require 'pry'
 
-art1 = Artist.new('Artist1', 10)
+art1 = Artist.new('Artist1', 20)
 art2 = Artist.new('Artist2', 20)
 art3 = Artist.new('Artist3', 30)
 
@@ -14,8 +14,11 @@ gal2 = Gallery.new('Gallery2', 'Moscow')
 gal3 = Gallery.new('Gallery3', 'Turkey')
 
 paint1 = Painting.new(1000, 'Mona Lisa', art1, gal1)
+paint1_2 = Painting.new(1200, 'Lona Misa', art1, gal2) 
 paint2 = Painting.new(2000, 'Rubens', art2, gal2)
+paint2_2 = Painting.new(4000, 'Rubensesque', art2, gal3)
 paint3 = Painting.new(3000, 'Da Vinci', art3, gal3)
+paint3_2 = Painting.new(5000, 'Da Vinci Code', art3, gal1)
 
 binding.pry
 
