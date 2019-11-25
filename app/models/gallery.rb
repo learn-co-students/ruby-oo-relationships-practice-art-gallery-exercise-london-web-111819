3c51e7ff1bd1358b4a9c44bafd0040f7f1a8f4c5
Lope Ariyo <lopeariyo@gmail.com>
2019-11-25 16:47:11 +0000

    Completed

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,35 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.filter {|painting| painting.gallery == self}
+  end
+
+  def artists
+    paintings.map {|painting| painting.artist }
+  end
+
+  def artist_names
+   artists.map {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    list_of_prices = paintings.map {|painting| painting.price}
+
+    list_of_prices.reverse[0]
+  end
+
 
 end
