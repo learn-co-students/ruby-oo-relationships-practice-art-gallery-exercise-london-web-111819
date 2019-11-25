9d111d6a0cc0b96f5fef8ae4e0d640270c9e3dd4
Matthew <matthew@Matthews-MacBook-Pro.local>
2019-11-25 09:11:17 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,41 @@
 class Artist
+  attr_reader :name
+  attr_accessor :total_experience
 
-  attr_reader :name, :years_experience
+  @@all = []
 
-  def initialize(name, years_experience)
+  def initialize(name, total_experience)
     @name = name
-    @years_experience = years_experience
+    @total_experience= total_experience
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    Artist.all.inject(0){|sum, artist| sum + artist.total_experience}
+  end
+
+  def self.most_prolific
+    # Still need to finish
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map{|painting| painting.gallery}
+  end
+
+  def cities
+    paintings.map{|painting| painting.gallery.city}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(price, title, self, gallery)
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,21 @@
 class Painting
-
-  attr_reader :title, :price
+  attr_reader :price, :title, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
-    @title = title
+  def initialize(price, title, artist, gallery)
     @price = price
+    @title = title
+    @artist = artist
+    @gallery = gallery
+    @@all.push(self)
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    Painting.all.inject(0){|sum, painting| sum + painting.price}
+  end
 end
