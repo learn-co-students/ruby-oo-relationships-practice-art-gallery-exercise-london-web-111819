c8f58fa70889eeb7a40e642c483edf1d80b83248
Sara Fotros <sarafotros@users.noreply.github.com>
2019-11-25 22:08:56 +0000

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,20 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting|painting.gallery == self}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,16 @@
 require_relative '../config/environment.rb'
 
 
+artist1 = Artist.new("Robert", 4)
+artist2 = Artist.new("Angelo" ,11)
 
+gal1 = Gallery.new("aka","NY")
+gal2 = Gallery.new("townhall", "Denver")
+gal3 = Gallery.new("main", "London")
+
+pic1 = Painting.new("face", 120, artist1, gal1)
+pic2 = Painting.new("Hands", 220, artist1, gal2)
+pic3 = Painting.new("Tree", 350, artist2, gal3)
 
 binding.pry
 
